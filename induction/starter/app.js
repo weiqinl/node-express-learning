b1e69df1184c30658c6f4072e86015e8be690532
liuweiqin <772863869@qq.com>
2016-09-16 15:18:17 +0800

    使用回调函数数组处理路由

--- induction/starter/app.js
+++ induction/starter/app.js
@@ -30,6 +30,20 @@ next(); //pass control to the next handler
   res.send('Hello from next()');
 });
 
+//使用回调函数数组处理路由：
+var cb0 = function(req, res, next) {
+  console.log('CB0');
+  next();
+};
+var cb1 = function(req, res, next) {
+  console.log('CB1');
+  next();
+};
+var cb2 = function(req, res, next) {
+	res.send('Hello from c!');
+};
+app.get('/example/c', [cb0, cb1, cb2]);
+
 var server = app.listen(3000, function() {
   var host = server.address().address;
   var port = server.address().port;
